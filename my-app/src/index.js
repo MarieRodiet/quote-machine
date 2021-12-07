494499bb4c73b4fac04eb1e414e88f5200d499a3
Marie Rodiet <marie.rodiet@gmail.com>
2021-12-07 20:43:20 +0100

    project completed

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,7 +6,6 @@ import App from './App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers.js/reducer';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { changeIndex } from "./action-creators";
 const store = createStore(reducer);
 
@@ -36,17 +35,3 @@ const AppWrapper = () => (
   </Provider>
 );
 ReactDOM.render(<AppWrapper />, document.getElementById('root'));
-
-
-/*
-REQUIREMENTS:
-1. I can see a wrapper element with a corresponding id="quote-box".
-2. Within #quote-box, I can see an element with corresponding id="text".
-3. Within #quote-box, I can see an element with corresponding id="author".
-4. Within #quote-box, I can see a clickable element with corresponding id="new-quote".
-5. Within #quote-box, I can see a clickable <a> element with corresponding id="tweet-quote".
-6. On first load, my quote machine displays a random quote in the element with id="text".
-7. On first load, my quote machine displays the random quote's author in the element with id="author".
-8. When the #new-quote button is clicked, my quote machine should fetch a new quote and display it in the #text element.
-9. My quote machine should fetch the new quote's author when the #new-quote button is clicked and display it in the #author element.
-10. I can tweet the current quote by clicking on the #tweet-quote <a> element. This <a> element should include the "twitter.com/intent/tweet" path in it's href attribute to tweet the current quote. */
